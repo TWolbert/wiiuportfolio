ed8589c0905e06a8d607d09d81312510bde5f3ae
TWolbert <teunwolbert@outlook.com>
2024-04-06 00:17:09 +0200

    Git updates to forget WSL zone identifiers

--- app/_components/bgVideo.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function BGVideo() {
-    return (
-        <video className="fixed top-0 left-0 z-0 w-full h-full object-cover" controls={false} autoPlay muted loop id="bgVideo">
-            <source src="/images/wiiuBGVideo.mp4" type="video/mp4" />
-        </video>
-    );
-}
--- app/_components/startupSoundPlayer.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function StartupSoundPlayer() {
-    return (
-        <audio id="startupPlayer">
-            <source src="/sounds/startup.mp3" type="audio/mpeg" />
-        </audio>
-    )
-}
